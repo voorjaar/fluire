915e7d5aefe580001e9f53a8c7bf9cba93fd4ebb
voorjaar <felitas@zoho.com>
2021-06-06 03:04:26 +0800

    feat: add navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,35 @@
+<template>
+  <nav class="navbar fixed top-0 w-full sm:pl-60" role="navigation" aria-label="main navigation">
+    <div class="border-b bg-white dark:bg-dark-900 dark:border-dark-300 shadow-sm w-full h-full my-auto flex px-4">
+      <h1 class="flex-grow dark:text-light-200 flex items-center font-serif text-lg tracking-wider">
+        Calendar
+      </h1>
+      <div class="flex gap-4 items-center">
+        <button class="nav-button" @click="toggle">
+          <i-bx:bx-moon v-show="isDark" />
+          <i-ion-ios-sunny v-show="!isDark" />
+        </button>
+
+        <button class="nav-button">
+          <i-ic:round-add-circle-outline />
+        </button>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style lang="postcss">
+  .navbar {
+    @apply relative z-30 h-16 md:(flex items-center);
+  }
+  .nav-button {
+    @apply rounded-lg bg-blue-gray-100 hover:bg-blue-gray-200 p-2 text-gray-700 dark:text-light-300 dark:bg-dark-700 dark:hover:bg-dark-500;
+  }
+</style>
+
+<script setup lang="ts">
+import { useToggle } from '@vueuse/core'
+import { isDark } from '../composables/dark'
+
+const toggle = useToggle(isDark)
+</script>
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import windi from 'vite-plugin-windicss'
+import components from 'vite-plugin-components'
+import icons, { ViteIconsResolver } from 'vite-plugin-icons'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    windi()
+    windi(),
+    components({
+      globalComponentsDeclaration: true,
+      customComponentResolvers: ViteIconsResolver()
+    }),
+    icons(),
   ]
 })
