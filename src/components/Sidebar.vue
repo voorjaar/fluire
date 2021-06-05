e55afc8ceea3d0653246dbcecca5bd45bbcef93f
voorjaar <felitas@zoho.com>
2021-06-05 21:43:56 +0800

    feat: add sidebar

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,43 @@
+<template>
+  <nav class="overflow-y-scroll w-full vstack p-3" in:fade>
+    <slot />
+    <div class="vstack select-none my-3" v-for="nav in navs">
+      <h2 class="text-gray-600 dark:text-gray-400 font-medium text-xs ml-1 mb-1">{{ nav.title }}</h2>
+      <div class="vstack">
+        <button v-for="child in nav.children" class="hstack rounded py-1.5 px-1 my-0.5 hocus:(bg-gray-100 bg-opacity-70 blur-5) dark:hocus:bg-dark-400">
+          <span class="text-blue-500 text-xl" />
+          <svg class="w-5 h-5 fill-blue-500 mr-2" viewBox="0 0 24 24"><path :d="child.icon" /></svg>
+          <span class="text-gray-900 dark:text-gray-100 text-sm align-bottom inline-block">{{ child.name }}</span>
+        </button>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+
+export interface NavItem {
+  id?: string,
+  name: string,
+  icon: string,
+}
+
+export interface Nav {
+  title: string;
+  children: NavItem[];
+};
+
+export default defineComponent({
+  props: {
+    navs: {
+      type: Array as PropType<Nav[]>,
+      required: true
+    }
+  },
+})
+</script>
+
+<style>
+
+</style>
--- windi.config.ts
+++ windi.config.ts
@@ -1,8 +1,38 @@
-import { defineConfig } from 'windicss/helpers'
+import scrollsnap from 'windicss/plugin/scroll-snap';
+import plugin from 'windicss/plugin';
+import colors from 'windicss/colors';
+import { defineConfig } from 'vite-plugin-windicss';
 
 export default defineConfig({
   darkMode: 'class',
-  // safelist: 'p-3 p-4 p-5',
   theme: {
+    extend: {
+      colors: {
+        gray: colors.blueGray,
+      },
+    },
   },
-})
+  shortcuts: {
+    hstack: 'flex items-center',
+    vstack: 'flex flex-col',
+    grow: 'flex-grow',
+    icon: 'w-6 h-6 fill-current',
+    'app-border': 'border-gray-200 dark:border-dark-300',
+    'app-modal': 'fixed top-0 w-full h-screen z-10 bg-white bg-opacity-70 blur-5 shadow-lg',
+  },
+  plugins: [
+    scrollsnap,
+    plugin(({ addBase, addVariant }) => {
+      addBase({
+        'button:focus, input:focus': {
+          'outline': 'none',
+        },
+      });
+      addVariant('hocus', ({ modifySelectors }) => {
+        return modifySelectors(({ className }) => {
+          return `.${className}:hover, .${className}:focus`;
+        });
+      });
+    }),
+  ],
+});
