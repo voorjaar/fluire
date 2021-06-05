64f989509ef061c81fc97b01271977022c9e07cc
voorjaar <felitas@zoho.com>
2021-06-05 19:19:36 +0800

    feat: add windicss

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
+  <img class="bg-red" alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <div class="bg-red-500">23</div>
 </template>
 
 <script lang="ts">
@@ -16,12 +17,5 @@ export default defineComponent({
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
